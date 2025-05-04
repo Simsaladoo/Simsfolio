59c119e2841864cea5486e1d9e30b5e5905ffcdf
Simsaladoo <simsaladoo@hotmail.com>
2025-05-03 21:31:14 -0700

    pathing

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -2,14 +2,14 @@
 const navbarHTML = `
 <nav>
     <ul>
-        <li><a href="https://github.com/Simsaladoo/Simsfolio/blob/main/index.html">Home</a></li>
-        <li><a href="https://david-miller.life/generic.html">Game Dev</a></li>
+        <li><a href="https://david-miller.life/index.html">Home</a></li>
+        <li><a href="https://david-miller.life/generic.html">Unreal Dev</a></li>
         <li>
             <a href="https://david-miller.life/elements.html">Programming</a>
             <ul class="dropdown">
-                <li><a href="#" id="content1">Unreal Python</a></li>
+                <li><a href="#" id="content1">Python</a></li>
                 <li><a href="#" id="content2">Tools</a></li>
-                <li><a href="#" id="content3">Ocean Buoyancy</a></li>
+                <li><a href="#" id="content1">Ocean Buoyancy</a></li>
                 <li><a href="#" id="content4">Image Data Layering</a></li>
             </ul>
         </li>
